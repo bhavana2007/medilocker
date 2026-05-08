a64cb0a5adbeed9174f06c43d1fba51864e3dd52
bhavana2007 <bhavanakolli983@gmail.com>
2026-05-08 09:57:40 +0530

    Initial MediLocker setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>MediLocker</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
